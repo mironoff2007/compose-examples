79f03ddd7d7f67f3fe8c0454a7f31ecef51dd4c5
Mironov Ury <mironoff2007@rambler.ru>
2022-12-04 20:44:06 +0300

    scrollable tabs indicator for pages

--- app/src/main/java/ru/mironov/compose_examples/ui/pager/Pager.kt
+++ app/src/main/java/ru/mironov/compose_examples/ui/pager/Pager.kt
@@ -1,29 +1,28 @@
 package ru.mironov.compose_examples.ui.pager
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.Color.Companion.Magenta
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.accompanist.pager.*
-import kotlinx.coroutines.*
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 
 @Preview
 @Composable
-fun TabsPreview(){
+fun TabsPreview() {
     Tabs()
 }
 
@@ -32,20 +31,22 @@ fun TabsPreview(){
 fun Tabs() {
     val pagerState = rememberPagerState()
 
-    val pages = listOf("1", "2", "3","4")
+    val pages = listOf("Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5", "Tab 6")
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        TabRow(
-            // Our selected tab is our current page
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ScrollableTabRow(
+            modifier = Modifier.wrapContentHeight(),
             selectedTabIndex = pagerState.currentPage,
-            // Override the indicator, using the provided pagerTabIndicatorOffset modifier
             indicator = { tabPositions ->
                 TabRowDefaults.Indicator(
                     Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
                 )
             }
         ) {
-            // Add tabs for all of our pages
             val scope = rememberCoroutineScope()
             pages.forEachIndexed { index, title ->
                 Tab(
@@ -53,27 +54,29 @@ fun Tabs() {
                     selected = pagerState.currentPage == index,
                     onClick = {
                         scope.launch {
-                            pagerState.animateScrollToPage(index)
+                            try {
+                                pagerState.animateScrollToPage(index)
+                            } catch (e: Exception) {
+                                Log.d("UI_tag", e.stackTraceToString())
+                            }
                         }
                     },
                 )
             }
         }
 
-        val colors = listOf(Red, Green, Blue, Yellow, Magenta)
-
         HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
             state = pagerState,
             count = Int.MAX_VALUE,
             userScrollEnabled = false
         ) { index ->
-            val page = index % pages.size
-            Text(
-                text = AnnotatedString("Page ${pagerState.currentPage + 1}"),
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(colors[page]),
-            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = AnnotatedString("Page ${pagerState.currentPage + 1}"), fontSize = 35.sp)
+            }
         }
     }
 
