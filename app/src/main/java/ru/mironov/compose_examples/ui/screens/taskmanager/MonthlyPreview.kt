91f796c08697641fc2d4b18b9ddfb1d1a96704a5
MSI_B5M <mironoff2007@ramler.ru>
2023-03-15 19:42:41 +0300

    init

---
+++ app/src/main/java/ru/mironov/compose_examples/ui/screens/taskmanager/MonthlyPreview.kt
@@ -0,0 +1,17 @@
+package ru.mironov.compose_examples.ui.screens.taskmanager
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true)
+fun MonthlyPreviewLayoutPreview() {
+    TaskManagerTheme {
+        MonthlyPreviewLayout()
+    }
+}
+
+@Composable
+fun MonthlyPreviewLayout() {
+
+}
